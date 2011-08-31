83b2e79eb438208140c5e676865d906db3a61f02
Ben Protas <ben@benprotas.com>
2011-08-31 13:39:20 -0400

    provide real simple interface to ItemAttributes elements via method_missing; for example, makes things like item.publication_date do the right thing if PublicationDate was part of the response group

--- lib/asin.rb
+++ lib/asin.rb
@@ -6,3 +6,27 @@ module ASIN
   autoload :Version,        'asin/version'
   autoload :Configuration,  'asin/configuration'
 end
+
+# We need these methods, but don't want to require Rails/ActiveSupport
+# Code based on ActiveSupport's Inflector class:
+# http://api.rubyonrails.org/classes/ActiveSupport/Inflector.html
+if !String.instance_methods.include? :camelize then
+  class String
+    def camelize
+        self.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+    end
+  end
+end
+
+if !String.instance_methods.include? :underscore then
+  class String
+    def underscore
+      word = this.to_s.dup
+      word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+      word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+      word.tr!("-", "_")
+      word.downcase!
+      word
+    end
+  end
+end
--- lib/asin/simple_item.rb
+++ lib/asin/simple_item.rb
@@ -1,4 +1,5 @@
 require 'hashie'
+require 'date'
 
 module ASIN
 
@@ -20,12 +21,8 @@ module ASIN
       @raw.ASIN
     end
 
-    def title
-      @raw.ItemAttributes!.Title
-    end
-
     def amount
-      @raw.ItemAttributes!.ListPrice!.Amount.to_i
+      self.list_price.Amount.to_i
     end
 
     def details_url
@@ -39,6 +36,34 @@ module ASIN
     def image_url
       @raw.LargeImage!.URL
     end
-  end
 
+    def parse_date(d)
+      Date.parse(d)
+    end
+
+    def method_missing(meth, *args)
+      # ItemAttributes only deals with camel-cased names, but we want to 
+      # accept "rubyfied" (snake_case) names as well:
+      normalized_meth = meth.to_s.camelize
+
+      # If we're dealing with something like "_date" when camalized
+      # and we have the attribute, parse the date; if we can't parse it, 
+      # throw a meaningful exception:
+      if normalized_meth =~ /.+_date$/ && @raw.ItemAttributes!.respond_to?(normalized_meth) then
+        self.parse_date(@raw.ItemAttributes!.send(normalized_meth))
+      elsif @raw.ItemAttributes!.respond_to?(normalized_meth)
+        @raw.ItemAttributes!.send(normalized_meth)
+      else
+        super
+      end
+    end
+
+    def respond_to?(name)
+      !!(@raw.ItemAttributes!.respond_to?(name.to_s.camelize) || super)
+    end
+
+    def attributes
+      @raw.ItemAttributes!.keys
+    end
+  end
 end
